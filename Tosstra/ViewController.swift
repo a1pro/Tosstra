8e4d550c098476a784bbc77b695a0f46ad1c5330
iosDeveloper555 <iosDeveloper555>
2020-07-02 09:31:25 +0530

    Initial Commit

---
+++ Tosstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tosstra
+//
+//  Created by Eweb on 02/07/20.
+//  Copyright © 2020 Eweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
